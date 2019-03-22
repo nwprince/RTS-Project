fe1c0eb56687be8676c48593b45ffbad27c6a05f
nwprince <nwprince@gmail.com>
2019-03-22 11:19:39 -0500

    Added svg icon support

--- web/public/src/components/actionbar/index.tsx
+++ web/public/src/components/actionbar/index.tsx
@@ -1,13 +1,14 @@
 import { Component, h } from "preact";
+import Icon from "../icon";
 import * as style from "./style.css";
 
 export default class Actionbar extends Component {
   public render() {
     return (
       <div class={style.actionbar}>
-        <button>Info</button>
-        <button>Play</button>
-        <button>Share</button>
+        {Icon("info")}
+        {Icon("play")}
+        {Icon("share")}
       </div>
     );
   }
