72349383c4c8ce91af863e838bb35c2bdd0c27b1
nwprince <nwprince@gmail.com>
2019-04-25 13:25:50 -0500

    Added checks for ffmpeg and ipfs

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import (
 	"github.com/nwprince/RTS-Project/cli"
 	"github.com/nwprince/RTS-Project/web"
+	"os/exec"
 )
+
+func init() {
+	// Check for dependencies
+	_, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		cli.PostStatus("error", "ffmpeg could not be found. Please make sure it is installed and in your PATH")
+	}
+	_, err = exec.LookPath("ipfs")
+	if err != nil {
+		cli.PostStatus("error", "IPFS could not be found. Please make sure it is installed and in your PATH")
+	}
+}
 
 func main() {
 	cli.PostStatus("app", "Initializing...")
